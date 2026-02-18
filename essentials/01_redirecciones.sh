b0365ed5f802df872cfbb839da4b3939c49c8c66
Tiago <tiago.zdo@gmail.com>
2026-02-18 14:50:53 -0300

    redirecciones

---
+++ essentials/01_redirecciones.sh
@@ -0,0 +1,20 @@
+
+Para crear una carpeta llamada lab_redirecciones
+```
+mkdir lab_redirecciones
+```
+
+Para entrar en la carpeta recién creada:
+```
+cd lab_redirecciones
+```
+
+Para ver la ruta completa del directorio actual:
+```
+pwd
+```
+
+
+
+
+
